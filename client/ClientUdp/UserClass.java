5ef2e3b70d596b8b40542fa171fff6cfa43fbab8
Samual Pinheiro <31665870+fsmpinheiro@users.noreply.github.com>
2022-06-17 14:56:56 -0300

    Init no lado Client

---
+++ client/ClientUdp/UserClass.java
@@ -0,0 +1,24 @@
+package ClientUdp;
+
+
+public class UserClass{
+	
+	public UserClass( ) {
+	}
+	
+	public void run( ) {
+		try {
+			
+			
+			
+			
+		}catch (Exception e) {
+			System.out.println( "UserClass.run( ): " + e.getMessage());
+		}
+	}
+	
+	
+	public static void main( String args[] ) {
+		System.out.println("Olá! Eu sou uma main( )");
+	}
+}
