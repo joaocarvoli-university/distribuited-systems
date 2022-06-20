abeb9f665ecd9a889209721d2a7250de4be4904f
João Victor <joaocarvoli@hotmail.com>
2022-06-20 16:24:14 -0300

    removing python client to test

--- server/server-udp.py
+++ server/server-udp.py
@@ -4,6 +4,6 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print('The server is ready to receive')
 while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
+    message, clientAddress = serverSocket.recvfrom(1024)
     print(message.decode())
     serverSocket.sendto(message, clientAddress)
