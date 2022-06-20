5372911bf3d83667c5cfcbd58b99e61179f61cc5
João Victor <joaocarvoli@hotmail.com>
2022-06-20 17:13:33 -0300

    adding diagrams

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project-networkTransparency
+
+
+![](/docs/CurrencyConverter%20Diagram.svg)
+![](/docs/Weather%20Diagram.svg)
--- server/udp.py
+++ server/udp.py
@@ -1,7 +1,10 @@
 from socket import *
+
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
+
 print('The server is ready to receive')
 while 1:
     message, clientAddress = serverSocket.recvfrom(1024)
