6bdbc4a5a778be26361e27eae29979d4c34ce353
Samual Pinheiro <31665870+fsmpinheiro@users.noreply.github.com>
2022-06-22 09:09:20 -0300

    Classes Updates - submit
    Updates nas classes ClientUPD e ServerUDP - socket e serversocket são instanciados, e esses recebem seus objetos Datagram.

--- client/client_Udp/ClientUDP.java
+++ client/client_Udp/ClientUDP.java
@@ -4,50 +4,46 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 
 
 //ClientUDP recebe de Despachante e envia para UDPServer
 public class ClientUDP{
 			
-	DatagramSocket aSocket;
+	Socket cSocket; 
+	DatagramSocket dataSocket;
 	DatagramPacket dataPacket;
 	InetAddress servip;
 	int serport;
 	String message;
 	
 	public ClientUDP( String serverHost, int portNumber ){
-			try {
-				this.aSocket = new DatagramSocket();
-				this.servip = InetAddress.getByName( serverHost );
-				this.serport = portNumber;
-
-			} catch ( Exception e){
-				System.out.println("ClientUPD Exception: " + e.getMessage());
-			}
+		try {
+			this.cSocket = new Socket(serverHost, portNumber );
+			this.dataSocket = new DatagramSocket();
 			
+		}catch ( Exception e) {
+			System.out.println("ClientUPD Exception: " + e.getMessage());
 		}
+	}
 	
 	
 	
 	//sendRequest( ) envia uma mensagem em bytes
 	public void sendRequest( String strMessage  ) {
 		try {
-			this.aSocket = new DatagramSocket();
 			this.message = strMessage;
 			
 			byte [] mByte = message.getBytes();
-			this.dataPacket = new DatagramPacket( mByte, message.length(), this.servip, this.serport);
+			this.dataPacket = new DatagramPacket(mByte, message.length(), this.cSocket.getInetAddress(), this.cSocket.getPort() );
 			
-			this.aSocket.send(dataPacket);
+			this.dataSocket.send( dataPacket );
+			this.dataPacket = null;
+			this.message = null;
+		} catch ( Exception e ) {
 			
-		} catch ( SocketException e ) {
-			System.out.println("ClientUPD SocketException: " + e.getMessage());
-		} catch ( IOException e ) {
-			System.out.println("ClientUPD IOException: " + e.getMessage());
-		}
-		
-		
+		}		
 	}
 	
 	//getResponse( ) recebe uma mensagem em bytes[1024]	
@@ -57,7 +53,7 @@ public class ClientUDP{
 
 			byte [] buffer = new byte[1024];
 			this.dataPacket = new DatagramPacket( buffer, buffer.length );
-			this.aSocket.receive(this.dataPacket);
+			this.dataSocket.receive(this.dataPacket);
 			inResponse = new String( dataPacket.getData() );
 				
 		} catch ( SocketException e ) {
--- client/server_test/ServerTest.java
+++ client/server_test/ServerTest.java
@@ -4,16 +4,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketException;
-
-import javax.sound.midi.Receiver;
 
 public class ServerTest{
 	
-	DatagramSocket sSocket;
-	DatagramPacket dataPacket;
-	
+	ServerSocket sSocket;
 	int serverPort = 7889;
 	boolean done;
 	
@@ -23,32 +18,37 @@ public class ServerTest{
 	
 	public void run( ) {
 		try {
-			this.sSocket = new DatagramSocket( serverPort );
-			byte [] buffer = new byte[1024];
+			this.sSocket = new ServerSocket ( serverPort );
+			DatagramSocket dataSocket = new DatagramSocket( sSocket.getLocalPort() );
 			
-			while( !done ) {
-				this.dataPacket = new DatagramPacket( buffer, buffer.length );
-				sSocket.receive( dataPacket );
+			
+			while( !this.done ) {
+				byte [] buffer = new byte[1024];
+				DatagramPacket dataPacket = new DatagramPacket( buffer, buffer.length );
+
+				dataSocket.receive( dataPacket );
 				
-				String message = new String( dataPacket.getData() );
+				String message = new String ( dataPacket.getData() );
 				System.out.println("Mensagem do cliente: " + message );
-				
+				message = null;
 				
 				String str = "recebido";
 				byte [] respMessage = str.getBytes();
+				
 				DatagramPacket response = new DatagramPacket( respMessage, respMessage.length, dataPacket.getAddress(), dataPacket.getPort() );
-				sSocket.send(response);
+				dataSocket.send( response );
+				
+				
 			}
-						
 		} catch ( SocketException e ) {
 			System.out.println( "SocketException error: " + e.getMessage() );
 		} catch ( IOException e) {
 			System.out.println( "IOException error: " + e.getMessage() );
-		} finally {
-			if( sSocket != null ) {
-				this.done = true;
-				sSocket.close();
-			}
+//		} finally {
+//			if( sSocket.isClosed() ) {
+//				this.done = true;
+//				sSocket.close();
+//			}
 		}
 	}
 	
@@ -59,66 +59,3 @@ public class ServerTest{
 		serv.run();
 	}
 }
-
-
-//private ServerSocket sSocket;
-//private boolean done;
-//
-//public ServerTest( ){
-//	done = false;
-//	
-//}
-//
-//
-//public void run( ) {
-//	try {
-//		sSocket = new ServerSocket( 7998 );			
-//		System.out.println("Servidor UPD iniciado, escutando na porta: " + sSocket.getLocalPort() + "...");
-//		
-//		while (!done) {
-//			Socket sClient = sSocket.accept();
-//			UDPSocket uSckt = new UDPSocket( sClient );
-//			uSckt.run();
-//			
-//		}
-//		
-//	} catch ( Exception e) {
-//		System.out.println("ServerTest.run error: " + e.getMessage());
-//	}
-//}
-//
-//
-//class UDPSocket implements Runnable{
-//	private Socket client;
-//	DatagramSocket sDatagram;
-//	
-//	public UDPSocket ( Socket client ) {
-//		try {
-//			this.client = client;
-//			this.sDatagram = new DatagramSocket();
-//		}catch( Exception e) {
-//			//TODO: error and exception on constructor;
-//		}
-//	}
-//	
-//	@Override
-//	public void run( ) {
-//		try {
-//			System.out.println("UDPSocket instanciado\n");
-//			//DatagramSocket sDatagram = new DatagramSocket();
-//			//sDatagram = new DatagramSocket();
-//			byte [ ] buffer = new byte[1024];
-//			DatagramPacket sDataPacket = new DatagramPacket(buffer, 1024);
-//			
-//			this.sDatagram.receive( sDataPacket );
-//			
-//			String received = new String( sDataPacket.getData(), 0 , sDataPacket.getLength() );
-//			
-//			this.sDatagram.close();
-//			
-//			System.out.println("Mensagem recebida: " + received  );
-//		}catch (Exception e ) {
-//			System.out.println("UDPSocket.run error: " + e.getMessage() );
-//		}
-//	}
-//}
