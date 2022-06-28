9e6cd5d35ac57d73b2d41cb65f89952901b7c055
João Victor <joaocarvoli@hotmail.com>
2022-06-27 21:49:22 -0300

    Configuring the dispatcher to choose the service

--- server/dispatcher.py
+++ server/dispatcher.py
@@ -1,12 +1,32 @@
-class dispatcher:
+import json
+import skeletons.Weather as sk_weather
+import skeletons.CurrencyConverter as sk_currency
+
+
+class Dispatcher:
     def __init__(self):
         print("do nothing")
 
-    def invoke(self, message: str) -> bool:
+    def invoke(message: json) -> bool:
         """
         This method is responsible to select which skeleton of a specific service will be used
         :param message: The client message send from the client
         :return: the response message
         """
-        message
+
+        if message['serviceName'] == 'CurrencyConverter':
+            del message['serviceName']
+            if message['methodName'] == 'convert':
+                del message['methodName']
+                sk_currency.CurrencyConverter.convert(message)
+            elif message['methodName'] == 'currency_exists':
+                del message['methodName']
+                sk_currency.CurrencyConverter.currency_exists(message)
+            elif message['methodName'] == 'currencies_available':
+                del message['methodName']
+                sk_currency.CurrencyConverter.currencies_available(message)
+
+        elif message['serviceName'] == 'WeatherAPI':
+            del message['serviceName']  # Reducing message size
+
         return False
