5fd30e9b379c8ec7dfab8eee106ab796ab90e042
João Victor <joaocarvoli@hotmail.com>
2022-07-08 19:36:33 -0300

    correcting

--- services/currencyConverter/CurrencyConverter.py
+++ services/currencyConverter/CurrencyConverter.py
@@ -12,7 +12,7 @@ class CurrencyConverter:
     def __init__(self, base: str):
         self.converter = Er.ExchangeRate(env['url_api'], base)
 
-    def convert(self, currency_from: str, currency_to: str, amount: float) -> float:
+    def convert(self, currency_from: str, currency_to: str, amount: float) -> dict:
         """
         This function convert the currency from some to others
         :param currency_from: A string that represents your actual currency, the currency that you have
@@ -23,17 +23,17 @@ class CurrencyConverter:
         from_currency = self.converter.get_rates()[currency_from]
         to_currency = self.converter.get_rates()[currency_to]
         conversion = (float(to_currency) * float(amount)) / float(from_currency)
-        return conversion
+        return {"amount": conversion}
 
-    def currency_exists(self, currency: str) -> bool:
+    def currency_exists(self, currency: str) -> dict:
         """
         This function checks if a specific currency exists
         :param currency: The acronyms of currency
         :return:A boolean that represents if this currency exists or not on used api
         """
         if currency in self.converter.get_rates():
-            return True
-        return False
+            return {"exists": True}
+        return {"exists": False}
 
     def currencies_available(self):
         """
